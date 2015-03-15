860d641318a14abac5253b07980ce2aaf29880ad
floud <ahalturin84@gmail.com>
2015-03-15 15:02:29 +0300

    Initial commit

---
+++ src/main/java/com/getjavajob/training/algo1503/halturina/lesson01/Task1_17.java
@@ -0,0 +1,24 @@
+package com.getjavajob.training.algo1503.halturina.lesson01;
+
+/**
+ * Created by Cloud on 15.03.2015.
+ */
+public class Task1_17 {
+
+    private static double x = 10;
+    private static double a = 1;
+    private static double b = 2;
+    private static double c = 3;
+
+    public static void main(String[] args) {
+
+        double result1 = Math.sqrt(1 - (1 - Math.cos(2*x))/2);  // o
+        double result2 = 1 / (Math.sqrt(a * Math.pow(x, 2) + b * x + c)); // p
+        double result3 = (Math.sqrt(x + 1) + Math.sqrt(x - 1)) / 2 * Math.sqrt(x); //r
+        double result4 = Math.abs(x) + Math.abs(x + 1); //s
+        System.out.println(result1 + " " + result2 + " " + result3 + " " + result4);
+
+
+    }
+
+}
