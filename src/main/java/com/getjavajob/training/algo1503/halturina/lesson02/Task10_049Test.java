529b09f295c29d5ae4202611f1d2605edceddef5
floud <ahalturin84@gmail.com>
2015-03-26 21:30:27 +0300

    Second

--- src/main/java/com/getjavajob/training/algo1503/halturina/lesson02/Task10_049Test.java
+++ src/main/java/com/getjavajob/training/algo1503/halturina/lesson02/Task10_049Test.java
@@ -1,7 +1,13 @@
 package com.getjavajob.training.algo1503.halturina.lesson02;
 
+import com.getjavajob.training.algo1503.halturina.util.Assert;
+
 /**
  * Created by floud on 26.03.2015.
  */
 public class Task10_049Test {
+    public static void main(String[] args) {
+        Task10_049.array = new int[]{5, 32, 44, 3, -4};
+        //Assert.assertEquals(2, Task10_049.getIndex(Task10_049.array.length));
+    }
 }
--- src/main/java/com/getjavajob/training/algo1503/halturina/lesson02/Task10_050Test.java
+++ src/main/java/com/getjavajob/training/algo1503/halturina/lesson02/Task10_050Test.java
@@ -1,7 +1,12 @@
 package com.getjavajob.training.algo1503.halturina.lesson02;
 
+import com.getjavajob.training.algo1503.halturina.util.Assert;
+
 /**
  * Created by floud on 26.03.2015.
  */
 public class Task10_050Test {
+    public static void main(String[] args) {
+        Assert.assertEquals(5, Task10_050.getAckermann(3, 1));
+    }
 }
